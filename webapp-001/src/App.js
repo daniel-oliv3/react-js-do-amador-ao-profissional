992bd7a1f1f1432b564480a4b250074fe25882be
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-28 13:47:33 -0300

    Update

--- webapp-001/src/App.js
+++ webapp-001/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           Olá Mundo!, Sapup3 na Aréa.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//Function components, jsx
+function App(){
+    return (
+        <div>
+            <h1>Olá Sapup3 Como Vai ?</h1>
+        </div>
+    );
+};
+
 
 export default App;
