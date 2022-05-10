323249e581fa84ab0a1cc102891691260efc1226
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-10 15:06:15 -0300

    Create project webapp-010

---
+++ webapp-010/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './App.css';
+
+//importação do componente, aula 18
+import ComponenteUm from "./components/ComponenteUm";
+import ComponenteDois from "./components/ComponenteDois";
+import ComponenteTres from "./components/ComponenteTres";
+import ComponenteQuatro from "./components/ComponenteQuatro";
+import ComponenteSeis from "./components/ComponenteSeis";
+import ComponenteSete from "./components/ComponenteSete";
+import ComponenteOito from "./components/ComponenteOito";
+import ComponenteNove from "./components/ComponenteNove";
+
+
+
+//19 - COMO DEFINIR ESTILOS INLINE COM STYLE NO JSX.
+function App(){
+    return (
+        <div>
+            <ComponenteNove titulo="Componente Nove" valor="Sapup3"/>
+            <ComponenteOito titulo="Comp 8" valor={10} valor="Sapup3"/> 
+            <ComponenteSete titulo="Comp 7" valor={1}/> 
+            <ComponenteSeis titulo="Componente 6"/>
+            <ComponenteUm titulo="1"/>
+            <ComponenteDois titulo="2"/>
+            <ComponenteTres titulo="3"/>
+            <ComponenteQuatro titulo="4"/>
+        </div>
+    );
+};
+
+
+export default App;
---
+++ webapp-010/src/dados_clientes/dados_clientes.js
@@ -0,0 +1,9 @@
+const dados_clientes = [
+    {id_cliente: 1, nome: "Sapup3", email: "sapup3webdeveloper@gmail.com"},
+    {id_cliente: 2, nome: "Yusuke", email: "yusuke-urameshi@gmail.com"},
+    {id_cliente: 3, nome: "Kuwabara", email: "kazuma-kuwabara@gmail.com"},
+    {id_cliente: 4, nome: "Kurama", email: "shuichi-minamino@gmail.com"},
+    {id_cliente: 5, nome: "Hiei", email: "hiei@gmail.com"},
+]
+
+export default dados_clientes;
