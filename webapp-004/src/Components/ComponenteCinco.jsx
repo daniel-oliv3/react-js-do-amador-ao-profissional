a4e85000d5c20a962ed8c11045e4d6f78fbc5c4c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-10 10:32:07 -0300

    Create project webapp-005

--- webapp-004/src/Components/ComponenteCinco.jsx
+++ webapp-004/src/Components/ComponenteCinco.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const ComponenteCinco = (props) => {
     return (
         <>
-            <p>Texto {props.texto}</p>
+            <p>Texto: - 5 {props.texto}</p>
         </>
     );
 }
