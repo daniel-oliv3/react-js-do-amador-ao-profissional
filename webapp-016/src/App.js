6a61c7d5abae022e2d101e766ef28cf6b882805c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-13 18:23:06 -0300

    Update

--- webapp-016/src/App.js
+++ webapp-016/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='componente'>
+        <h1>React Hooks</h1>
+      </div>
+    </>
   );
 }
-
-export default App;
