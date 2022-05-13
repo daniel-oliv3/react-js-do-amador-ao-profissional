64c138da7722504c1cc7aed7386c0f7423191ec4
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-13 18:36:28 -0300

    Update

--- webapp-016/src/App.js
+++ webapp-016/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+
 
 
 export default function App(){
+
+  //console.table(useState(10));
+
+  const [valor, setValor] = useState(10);
+
+  function decremento(){
+    setValor(valor - 1);
+  }
+
+  function incremento(){
+    setValor(valor + 1);
+  }
+
   return (
     <>
-      <div className='componente'>
+
         <h1>React Hooks</h1>
-      </div>
+        <p>Valor: {valor}</p>
+
+        <button onClick={decremento}>Decremento</button>
+        <button onClick={incremento}>Incremento</button>
     </>
   );
 }
