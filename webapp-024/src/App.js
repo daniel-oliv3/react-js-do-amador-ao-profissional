eae68b9d1c8988d8b471e6855f5b2043c22e7c80
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-21 11:03:42 -0300

    Create project webapp-024

---
+++ webapp-024/src/App.js
@@ -0,0 +1,17 @@
+
+
+
+export default function App(){
+
+  return (
+    <>
+        <h1>31 - EXERCÍCIO DE LEITURA DE DADOS DE API NO USEEFFECT</h1>
+        <hr/>
+
+        
+        
+
+        
+    </>
+  );
+}
