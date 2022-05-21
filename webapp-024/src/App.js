becec27c7d0d1b8f781f9cae6942bcf9a6fd63c3
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-21 11:37:48 -0300

    Update

--- webapp-024/src/App.js
+++ webapp-024/src/App.js
@@ -1,12 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 
 export default function App(){
 
+  //state
+  const [users, setUsers] = useState([]);
+
+  //Efect
   useEffect(() => {
     fetch("https://dummyjson.com/users")
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => {
+      console.log(json.users)
+      setUsers(json.users)
+    })
   }, []);
 
   return (
@@ -14,7 +21,9 @@ export default function App(){
         <h1>31 - EXERCÍCIO DE LEITURA DE DADOS DE API NO USEEFFECT</h1>
         <hr/>
 
-        
+        {users.map(user => {
+          return <div key={user.id}>{user.firstName} {user.lastName}</div>
+        })}
         
 
         
