ff4c1e8fd5880b28dc5ac631c1d81f4aa06b38c4
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-25 09:23:19 -0300

    Update

--- webapp-026/src/App.js
+++ webapp-026/src/App.js
@@ -5,21 +5,32 @@ import './App.css';
 export default function App(){
 
   const [texto, setTexto] = useState('');
-  const total = useRef(1);
+  const inputRef = useRef();
+  const input2Ref = useRef();
 
-  useEffect(() => {
-    total.current++;
-  })
+  function focus1(){
+    //console.log(inputRef.current.value);
+    //console.log(texto);
+    inputRef.current.focus();
+  }
+
+  function alterar_texto2(){
+    input2Ref.current.value = "Daniel Oliveira";
+    input2Ref.current.focus();
+  }
+
 
   return (
     <>
-        <h1>32 - INTRODUÇÃO AO USEREF</h1>
+        <h1>33 - UTILIZAÇÃO MAIS COMUM DO USEREF</h1>
         <hr/>
         
-        <input type="text" value={texto} onChange={evento => { setTexto(evento.target.value)}} />
-        <p>O texto é: <strong>{texto}</strong></p>
+        <input ref={inputRef} type="text" value={texto} onChange={evento => { setTexto(evento.target.value)}} /><br/>
 
-        <p>Total: {total.current}</p>
+        <input ref={input2Ref} type="text" /><br/>
+
+        <button onClick={focus1}>Focus 1</button>
+        <button onClick={alterar_texto2}>Alterar o texto do input 2</button>
     </>
   );
 }
