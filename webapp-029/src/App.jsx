59b251bf50ebdd964b2d2a48880d277f73379e09
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-28 10:14:01 -0300

    Up

--- webapp-029/src/App.jsx
+++ webapp-029/src/App.jsx
@@ -12,6 +12,23 @@ export default function App(){
   //Ref
   const idTarefa = useRef(0);
 
+
+  //Metodos
+  function adicionarTarefa(){
+    setListaTarefas(old => { return [...old, {id: idTarefa.current, texto: tarefa }]});
+    idTarefa.current = idTarefa.current + 1;
+  }
+
+  function limparTarefas(){
+    setListaTarefas(old => { return [] } );
+    idTarefa.current = 0;
+  }
+
+  function removerTarefa(id){
+    const tmp = listaTarefas.filter(tarefa => tarefa.id !== id );
+    setListaTarefas(tmp);
+  }
+
   return (
     <>
         <h1>Gestor de Tarefas</h1>
@@ -20,11 +37,14 @@ export default function App(){
         <input type="text" value={tarefa} onChange={(evento) => { setTarefa(evento.target.value) }}/>
         <div>
           <button onClick={adicionarTarefa}>Adicionar</button>
-          <button>Limpar Tudo</button>
+          <button onClick={limparTarefas}>Limpar Tudo</button>
         </div>
         <hr/>
 
         <p>Tarefas: </p>
+        {listaTarefas.map((tarefa) => {
+          return <p key={tarefa.id}>{tarefa.texto} - <span onClick={() => { removerTarefa(tarefa.id) } }>[remover]</span></p>
+        })}
     </>
   );
 }
