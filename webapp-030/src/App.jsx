1475ccd47a9ebde6bad304ef83814854178c24b4
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-30 20:32:38 -0300

    Update

--- webapp-030/src/App.jsx
+++ webapp-030/src/App.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
-
   
 
 export default function App(){
 
-  
+  const [valor, setValor] = useState(() => { return 0 });
+  const [numero, setNumero] = useState(() => { return 1000 });
+
+  function incrementar(){
+    setValor((oldValor) => oldValor + 1);
+  }
 
   return (
     <>
         <h1>37 - USEMEMO</h1>
         <hr/>
 
+        <p>Valor: {valor}</p>
+        <button onClick={incrementar}>Incrementar</button>
     </>
   );
 }
