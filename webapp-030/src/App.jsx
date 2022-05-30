e0ff93d5fef308e9c534f6d8b35853a25f098998
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-30 20:47:22 -0300

    Update

--- webapp-030/src/App.jsx
+++ webapp-030/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import './App.css';
   
 
@@ -7,8 +7,28 @@ export default function App(){
   const [valor, setValor] = useState(() => { return 0 });
   const [numero, setNumero] = useState(() => { return 1000 });
 
+  //const valorCalculado = funcaoDemorada(valor);
+  const valorCalculado = useMemo(() => {
+    return funcaoDemorada(valor);
+  }, [valor]);
+
+  useEffect(() => {
+    console.log('render');
+  });
+
   function incrementar(){
     setValor((oldValor) => oldValor + 1);
+  }
+
+  function incrementar2(){
+    setNumero((oldNumero) => oldNumero + 100);
+  }
+
+  function funcaoDemorada(num){
+
+    for(let i=0; i<1000000000; i++){}
+
+    return num * 2;
   }
 
   return (
@@ -17,7 +37,10 @@ export default function App(){
         <hr/>
 
         <p>Valor: {valor}</p>
+        <p>Número: {numero}</p>
+        <p>Valor Calculado: {valorCalculado}</p>
         <button onClick={incrementar}>Incrementar</button>
+        <button onClick={incrementar2}>Incrementar 2</button>
     </>
   );
 }
