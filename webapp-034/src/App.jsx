137904ae9adb67dd8ef6cb04bbff13e5719f6c17
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-02 19:30:08 -0300

    Up

--- webapp-034/src/App.jsx
+++ webapp-034/src/App.jsx
@@ -1,44 +1,15 @@
-import React, { useReducer } from "react";
+import React from "react";
 import './App.css';
   
 
 export default function App(){
-
- //Reducer
- const [state, dispatch] = useReducer(reducer, {
-   valor: 1000,
-   mostrar: true
- });
-
- //Função reducer
- function reducer(state, action){
-    switch(action.type){
-      case 'INCREMENTAR':
-        return {
-          valor: state.valor + 1,
-          mostrar: state.mostrar
-        }
-
-      case 'VISIBILIDADE':
-          return {
-            valor: state.valor,
-            mostrar: !state.mostrar
-          }
-      default:
-        return state  
-    }
- }
 
   return (
     <>
         <h1>40 - EXERFCÍCIO PRÁTICO COM USEREDUCER</h1>
         <hr/>
         
-        <p>Valor: { state.valor }</p>
-        {state.mostrar && <p>Colocar visivel</p>}
 
-        <button onClick={() => { dispatch({type: "INCREMENTAR"}) }}>Incrementar</button>
-        <button onClick={() => { dispatch({type: "VISIBILIDADE"}) }}>Mostrar/Esconder</button>
     </>
   );
 }
