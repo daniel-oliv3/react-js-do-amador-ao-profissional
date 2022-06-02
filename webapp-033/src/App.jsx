bfe05937abf37d740d8b919aafb82f12dea75b45
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-02 09:11:32 -0300

    Update useReducer

--- webapp-033/src/App.jsx
+++ webapp-033/src/App.jsx
@@ -1,22 +1,44 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import './App.css';
   
 
 export default function App(){
 
-  const [valor, setValor] = useState(100);
-  const [mostrar, setMostrar] = useState(false);
+ //Reducer
+ const [state, dispatch] = useReducer(reducer, {
+   valor: 1000,
+   mostrar: true
+ });
+
+ //Função reducer
+ function reducer(state, action){
+    switch(action.type){
+      case 'INCREMENTAR':
+        return {
+          valor: state.valor + 1,
+          mostrar: state.mostrar
+        }
+
+      case 'VISIBILIDADE':
+          return {
+            valor: state.valor,
+            mostrar: !state.mostrar
+          }
+      default:
+        return state  
+    }
+ }
 
   return (
     <>
         <h1>39 - USEREDUCER</h1>
         <hr/>
-        <p>Valor: {valor}</p>
-        {mostrar && <p>Colocar visível</p>}
-
-        <button onClick={() => { setValor(valor + 1) }}>Incrementar</button>
-        <button onClick={() => { setMostrar(!mostrar) }}>Mostrar/Esconder</button>
         
+        <p>Valor: { state.valor }</p>
+        {state.mostrar && <p>Colocar visivel</p>}
+
+        <button onClick={() => { dispatch({type: "INCREMENTAR"}) }}>Incrementar</button>
+        <button onClick={() => { dispatch({type: "VISIBILIDADE"}) }}>Mostrar/Esconder</button>
     </>
   );
 }
