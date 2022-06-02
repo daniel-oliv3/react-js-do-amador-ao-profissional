766a731b9f9e4d0fd0e487407e47be7df3ca9d78
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-02 08:34:17 -0300

    Up

--- webapp-032/src/App.jsx
+++ webapp-032/src/App.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
   
 
 export default function App(){
 
+  const [valor, setValor] = useState(100);
+  const [mostrar, setMostrar] = useState(true);
 
   return (
     <>
         <h1>39 - USEREDUCER</h1>
         <hr/>
-
+        <p>Valor: {valor}</p>
+        {mostrar && <p>Colocar visível</p>}
         
     </>
   );
