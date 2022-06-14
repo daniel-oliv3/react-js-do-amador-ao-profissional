b58ed1af47569a500afc68deacc1ef96e01f8183
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-13 22:01:47 -0300

    Update App.jsx

--- webapp-039/src/App.jsx
+++ webapp-039/src/App.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import './App.css';
   
 
 export default function App(){
 
+  const [numero, setNumero] = useState(100);
+  const [temaEscuro, setTemaEscuro] = useState(false);
+  
+//Função para calcular lista de resultados
+const calculos = () => {
+  return [numero*2, numero*3, numero*4];
+}
+
+//Tema app
+cost tema = {
+  backgroundColor: temaEscuro ? "black" : "white",
+  color: temaEscuro ? "white" : "black",
+  height: "100vh"
+}
 
   return (
     <>
