8be98bd26109b683a14adce618b609f9aa54a44b
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-15 15:02:30 -0300

    Update App.jsx

--- webapp-040/src/App.jsx
+++ webapp-040/src/App.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {useId} from "react";
 import './App.css';
   
 
 export default function App(){
 
+  const id1 = useId();
+  const id2 = useId();
 
   return (
     <>
         <h1>45 - USEID</h1>
         <hr/>
 
-        
+        <form action="#" method="post">
+          <div>
+            <input type="checkbox" name="checkbox_masculino" id={id1} />
+            <label htmlFor={id1}>Texto da Checkbox 1</label>
+          </div>
+          <div>
+            <input type="checkbox" name="checkbox_masculino" id={id2} />
+            <label htmlFor={id2}>Texto da Checkbox 2</label>
+          </div>
+        </form>
     </>
   );
 }
