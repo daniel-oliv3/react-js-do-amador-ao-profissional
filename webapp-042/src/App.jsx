3f335aa4d15601ffe1807bf1a16351133969978c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-17 12:01:42 -0300

    Create project webapp-042

---
+++ webapp-042/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+  
+
+export default function App(){
+
+
+  return (
+    <>
+        <h1>46 - VÁRIOS COMPONENTES NO MESMO FICHEIRO</h1>
+        <hr/>
+
+        
+    </>
+  );
+}
