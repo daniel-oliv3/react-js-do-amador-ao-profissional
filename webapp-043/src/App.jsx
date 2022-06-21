68271c223aa63eed6920588c0b074aacd1137108
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-20 21:27:51 -0300

    Update App.jsx

--- webapp-043/src/App.jsx
+++ webapp-043/src/App.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
   
 
 export default function App(){
+
+  const [valor, setValor] = useState(0);
+
+  /*Functions*/
+  function decremento(){
+    setValor(oldValor => oldValor - 1);
+  }
+
+  function incremento(){
+    setValor(oldValor => oldValor + 1);
+  }
 
 
   return (
@@ -10,7 +21,9 @@ export default function App(){
         <h1>48 - PASSAR APLICAÇÃO PARA PRODUÇÃO E HOSPEDAR NUM SERVIDOR WEB</h1>
         <hr/>
 
-        
+        <p>Valor: <strong>{valor}</strong></p>
+        <button onClick={decremento}>Decremento</button>
+        <button onClick={incremento}>Incremento</button>
     </>
   );
 }
