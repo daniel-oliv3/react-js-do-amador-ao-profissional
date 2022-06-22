4ece7cef26b2ce3418f4d2ff71e8a1cca9a700b6
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-21 21:33:01 -0300

    Update App.jsx

--- webapp-044/src/App.jsx
+++ webapp-044/src/App.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
   
 
 export default function App(){
 
+  //states
+  const [contato, setContato] = useState({nome: '', telefone: ''});
+
+  const [listaContatos, setListaContatos] = useState([]);
+
+  //Metodos/funções
+  function definirNome(event){
+    setContato({...contato, nome: event.target.value});
+  }
 
   return (
     <>
         <h1>49 - EXERCÍCIO PRÁTICO INÍCIO DA LISTA DE CONTACTOS</h1>
         <hr/>
 
-        
+        <div>
+          <input type="text" onChange={definirNome} value = {contato.nome} />
+        </div>
+        <hr />
+
+        <p>{contato.nome}</p>
+        {/* Render da lista de contatos */}
     </>
   );
 }
