6e38545e52b79ca7614dd906a21127f913943246
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-24 21:09:43 -0300

    Create project webapp-047

---
+++ webapp-047/src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import ListaContatos from "./Components/ListaContatos";
+import './App.css';
+  
+
+export default function App(){
+
+  //states
+  const [contato, setContato] = useState({nome: '', telefone: ''});
+
+  const [listaContatos, setListaContatos] = useState([]);
+
+  //Metodos/funções
+  function definirNome(event){
+    setContato({...contato, nome: event.target.value});
+  }
+
+  function definirTelefone(event){
+    setContato({...contato, telefone: event.target.value});
+  }
+
+  function adicionarContato(){
+    setListaContatos([...listaContatos, contato]);
+    console.table(listaContatos);
+  }
+
+  return (
+    <>
+        <h1>52 - EXERCÍCIO PRÁTICO VALIDAÇÕES E USEREF PARA FOCUS</h1>
+        <hr/>
+
+        <div>
+          <label>Nome: </label><br/>
+          <input type="text" onChange={definirNome} value = {contato.nome} />
+        </div>
+        <div>
+          <label>Telefone: </label><br/>
+          <input type="text" onChange={definirTelefone} value = {contato.telefone} />
+        </div>
+        
+
+        {/*<p>{contato.nome}</p>
+        <p>{contato.telefone}</p>*/}
+        {/* Render da lista de contatos */}
+
+        <button onClick={adicionarContato}>Adicionar Contato</button>
+
+        <hr />
+
+        <ListaContatos listaContatos={listaContatos}/>
+    </>
+  );
+}
