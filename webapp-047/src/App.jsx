69334959cae9b99b23dd8bbc1339b5f0b7f6a0a3
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-28 09:22:22 -0300

    Update

---
+++ webapp-047/src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import Contato from "./Components/Contato";
+import { v4 as chave } from "uuid";
+import "./App.css";
+
+export default function App() {
+  //states
+  const [contato, setContato] = useState({ nome: "", telefone: "" });
+
+  const [listaContatos, setListaContatos] = useState([]);
+
+  //Metodos/funções
+  function definirNome(event) {
+    setContato({ ...contato, nome: event.target.value });
+  }
+
+  function definirTelefone(event) {
+    setContato({ ...contato, telefone: event.target.value });
+  }
+
+  function adicionarContato() {
+    /*Validação dos campos*/
+    if (contato.nome === "" || contato.telefone === "") {
+      //console.log('return');
+      return;
+    }
+
+    /*Adicionar novo contato a lista*/
+    setListaContatos([...listaContatos, contato]);
+  }
+
+  return (
+    <>
+      <h1>52 - EXERCÍCIO PRÁTICO VALIDAÇÕES E USEREF PARA FOCUS</h1>
+      <hr />
+
+      <div>
+        <label>Nome: </label>
+        <br />
+        <input type="text" onChange={definirNome} value={contato.nome} />
+      </div>
+      <div>
+        <label>Telefone: </label>
+        <br />
+        <input
+          type="text"
+          onChange={definirTelefone}
+          value={contato.telefone}
+        />
+      </div>
+
+      <button onClick={adicionarContato}>Adicionar Contato</button>
+
+      <hr />
+
+      {/* Apresentação da lisa de contatos */}
+      <>
+        {listaContatos.map((ct) => {
+          return <Contato key={chave} nome={ct.nome} telefone={ct.telefone} />;
+        })}
+      </>
+    </>
+  );
+}
